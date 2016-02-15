830787fb27772e6e544518aba831e97a61bc2fe1
chris30269 <cernst3@gatech.edu>
2016-02-15 15:27:09 -0500

    Initial Commit!
    It’s alive! (for certain definitions of “alive”)

---
+++ js.js
@@ -0,0 +1,87 @@
+var freqs = [440, 466.164, 493.883, 523.252, 554.366, 293.665, 311.127, 329.628, 349.228, 369.994, 391.995, 415.305];
+var notes = ["A", "A#/Bb", "B",     "C",     "C#/Db", "D",     "D#/Eb", "E",     "F",     "F#/Gb", "G",     "G#"];
+var audioContext = new AudioContext();
+var tonic = 440;
+var cents = 0;
+
+$(function(){
+
+	detector = new PitchDetector({
+	    // Audio Context (Required)
+	    context: audioContext,
+
+	    // Input AudioNode (Required)
+	    //input: null, // default: Microphone input
+
+	    // Output AudioNode (Optional)
+	    //output: AudioNode, // default: no output
+
+	    // interpolate frequency (Optional)
+	    //
+	    // Auto-correlation is calculated for different (discrete) signal periods
+	    // The true frequency is often in-beween two periods.
+	    //
+	    // We can interpolate (very hacky) by looking at neighbours of the best 
+	    // auto-correlation period and shifting the frequency a bit towards the
+	    // highest neighbour.
+	    interpolateFrequency: true, // default: true
+
+	    // Callback on pitch detection (Optional)
+	    onDetect: function(stats, pitchDetector) { 
+	        stats.frequency // 440
+	        stats.detected // --> true
+	        stats.worst_correlation // 0.03 - local minimum, not global minimum!
+	        stats.best_correlation // 0.98
+	        stats.worst_period // 80
+	        stats.best_period // 100
+	        stats.time // 2.2332 - audioContext.currentTime
+	        stats.rms // 0.02 
+
+	        if(stats.detected){
+	        	//var pitch = constrainPitch();
+	        	$("#hz").html(stats.frequency);
+	        	var fraction = Math.log(stats.frequency/tonic)/Math.log(2);
+	        	if (fraction > 1) fraction = fraction%1;
+	        	$("#ballcircle").css("transform", "rotate("+fraction*360.0+"deg)");
+	        	$("#cents").html(1200.0*fraction);
+	        }
+	    },
+
+	    // Debug Callback for visualisation (Optional)
+	    onDebug: function(stats, pitchDetector) { },
+
+	    // Minimal signal strength (RMS, Optional)
+	    minRms: 0.01,
+
+	    // Detect pitch only with minimal correlation of: (Optional)
+	    minCorrelation: 0.9,
+
+	    // Detect pitch only if correlation increases with at least: (Optional)
+	    //minCorreationIncrease: 0.5,
+
+	    // Note: you cannot use minCorrelation and minCorreationIncrease
+	    // at the same time!
+
+	    // Signal Normalization (Optional)
+	    normalize: "rms", // or "peak". default: undefined
+
+	    // Only detect pitch once: (Optional)
+	    stopAfterDetection: false,
+
+	    // Buffer length (Optional)
+	    length: 1024, // default 1024
+
+	    // Limit range (Optional):
+	    //minNote: 69, // by MIDI note number
+	    //maxNote: 80, 
+
+	    minFrequency: 80,    // by Frequency in Hz
+	    maxFrequency: 20000,
+
+	    minPeriod: 2,  // by period (i.e. actual distance of calculation in audio buffer)
+	    maxPeriod: 512, // --> convert to frequency: frequency = sampleRate / period
+
+	    // Start right away
+	    start: true // default: false
+	});
+});
