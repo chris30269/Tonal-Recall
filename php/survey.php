b1fbadfaa47563d63f7276fee70f91bf7a2c7fd5
Chris Ernst <cernst3@gatech.edu>
2016-03-26 18:50:46 -0400

    save and load survey results

---
+++ php/survey.php
@@ -0,0 +1,33 @@
+<?php
+include 'connect.php';
+
+if($link){
+	$userId = $_POST["userId"];
+	$responses = $_POST["responses"];
+	if($_POST["action"] == "save"){
+		$sql = 'UPDATE `userData` SET `survey`=\''.$responses.'\' WHERE `userId`='.$userId.';';
+		if (mysqli_query($link, $sql)) {
+			echo "success";
+
+		    /* free result set */
+		    mysqli_free_result($result);
+		}
+	}
+
+	if($_POST["action"] == "load"){
+		$sql = "SELECT `survey` FROM `userData` WHERE `userId`=$userId;";
+		if ($result = mysqli_query($link, $sql)) {
+			$row = mysqli_fetch_assoc($result);
+			echo $row["survey"];
+
+		    /* free result set */
+		    mysqli_free_result($result);
+		}
+	}
+	
+}
+else{
+	echo "not connected";
+}
+
+?>
