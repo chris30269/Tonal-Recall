bb5e18644f94ae7e81f0738d903ac53729e6b8e4
Chris Ernst <cernst3@gatech.edu>
2016-04-15 22:10:23 -0400

    hella basic stats

--- stats.js
+++ stats.js
@@ -1,6 +1,12 @@
 var allData;
 var timeToTarget={};
 
+//time to do each assignment
+
+//time to hit each target
+
+//variance in hitting each target
+
 $(function(){
 	$.post( "php/stats.php", function(data) {
 		allData = JSON.parse(data);
@@ -9,19 +15,34 @@ $(function(){
 });
 
 function timeToTargets(){
+	// for (var i = 0; i < allData.length; i++) {
+	// 	//for each user
+	// 	for (var j = 0; j < allData[i].data.length; j++) {
+	// 		//for each user's data
+	// 		if(!timeToTarget[i]) timeToTarget[i]=[];
+	// 		//timeToTarget[j].push(allData[i].data[j]);
+	// 		if(!timeToTarget[i][allData[i].data[j].assignment-1]) timeToTarget[i][allData[i].data[j].assignment-1] = [];
+	// 		//timeToTarget[allData[i].data[j].assignment].push(allData[i].data[j].);
+	// 		for (var k = 0; k < allData[i].data[j].attempts.length; k++) {
+	// 			//for each user's notes in each assignment
+	// 			if(!timeToTarget[i][allData[i].data[j].assignment-1][k]) timeToTarget[i][allData[i].data[j].assignment-1][k] = [];
+	// 			// timeToTarget[i][j][k].push(allData[i].data[j].attempts[k]);
+	// 		};
+	// 	};
+	// };
+	var count = 0;
 	for (var i = 0; i < allData.length; i++) {
-		//for each user
+		if(allData[i].data != "" && allData[i] != "new" && allData[i].userId > 24){
+			count++;
+		}
+		var string = "<tr><td>"+allData[i].userId+"</td>";
 		for (var j = 0; j < allData[i].data.length; j++) {
-			//for each user's data
-			if(!timeToTarget[i]) timeToTarget[i]=[];
-			//timeToTarget[j].push(allData[i].data[j]);
-			if(!timeToTarget[i][allData[i].data[j].assignment-1]) timeToTarget[i][allData[i].data[j].assignment-1] = [];
-			//timeToTarget[allData[i].data[j].assignment].push(allData[i].data[j].);
-			for (var k = 0; k < allData[i].data[j].attempts.length; k++) {
-				//for each user's notes in each assignment
-				if(!timeToTarget[i][allData[i].data[j].assignment-1][k]) timeToTarget[i][allData[i].data[j].assignment-1][k] = [];
-				// timeToTarget[i][j][k].push(allData[i].data[j].attempts[k]);
-			};
+			string += "<td>"+allData[i].data[j].assignment+"</td>";
 		};
+		string += "</tr>";
+		$("#table").append(string);
 	};
+
+	$("#totalUsers").html(allData.length);
+	$("#activeUsers").html(count);
 }
