2cf8953961cb37966ea8930fd2143be8b401ed77
chris30269 <cernst3@gatech.edu>
2016-04-20 16:14:36 -0400

    baby's first d3 graph

--- stats.js
+++ stats.js
@@ -11,6 +11,7 @@ $(function(){
 	$.post( "php/stats.php", function(data) {
 		allData = JSON.parse(data);
 		timeToTargets();
+		whenJoined();
 	});
 });
 
@@ -47,4 +48,83 @@ function timeToTargets(){
 
 	$("#totalUsers").html(allData.length);
 	$("#activeUsers").html(count);
+}
+
+function whenJoined(){
+	var joined = {};
+	for (var i = 0; i < allData.length; i++) {
+		var date = new Date(1000*allData[i].consent);
+		var month = date.getMonth()+1;
+		var day = date.getDate();
+		var year = date.getFullYear();
+		var datestring = ""+month+" "+day+" "+year;
+		if(joined[datestring]) joined[datestring] = joined[datestring]+1;
+		else joined[datestring] = 1;
+	};
+	var joinedArr = [];
+	for (var i = 0; i < Object.keys(joined).length; i++) {
+		var date = Object.keys(joined)[i];
+		var num = joined[date];
+		var temp = {"date":date, "num":num};
+		joinedArr.push(temp);
+	};
+	// console.log(JSON.stringify(joinedArr));
+
+	var margin = {top: 20, right: 20, bottom: 30, left: 40},
+	    width = 960 - margin.left - margin.right,
+	    height = 500 - margin.top - margin.bottom;
+
+	var x = d3.scale.ordinal()
+	    .rangeRoundBands([0, width], .1);
+
+	var y = d3.scale.linear()
+	    .range([height, 0]);
+
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left")
+	    .ticks(10, "");
+
+	var svg = d3.select("body").append("svg")
+	    .attr("width", width + margin.left + margin.right)
+	    .attr("height", height + margin.top + margin.bottom)
+	  .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	var data = joinedArr;
+	x.domain(data.map(function(d) { return d.date; }));
+	y.domain([0, d3.max(data, function(d) { return d.num; })]);
+
+	svg.append("g")
+	    .attr("class", "x axis")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(xAxis);
+
+	svg.append("g")
+	    .attr("class", "y axis")
+	    .call(yAxis)
+	  .append("text")
+	    .attr("transform", "rotate(-90)")
+	    .attr("y", 6)
+	    .attr("dy", ".71em")
+	    .style("text-anchor", "end")
+	    .text("New users");
+
+	svg.selectAll(".bar")
+	    .data(data)
+	  .enter().append("rect")
+	    .attr("class", "bar")
+	    .attr("x", function(d) { return x(d.date); })
+	    .attr("width", x.rangeBand())
+	    .attr("y", function(d) { return y(d.num); })
+	    .attr("height", function(d) { return height - y(d.num); });
+
+	function type(d) {
+	  d.num = +d.num;
+	  return d;
+	}
 }
