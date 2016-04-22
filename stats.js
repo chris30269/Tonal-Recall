9f1271a5e06749bcc5d5f9775ae05cde54be8811
chris30269 <cernst3@gatech.edu>
2016-04-22 19:49:57 -0400

    time to target and time to assignment graphs (i think... math is hard)

--- stats.js
+++ stats.js
@@ -1,5 +1,5 @@
 var allData;
-var timeToTarget={};
+var lastDevUser = 24;
 
 //time to do each assignment
 
@@ -10,13 +10,15 @@ var timeToTarget={};
 $(function(){
 	$.post( "php/stats.php", function(data) {
 		allData = JSON.parse(data);
-		timeToTargets();
+		people();
 		whenJoined();
 		assignmentsCompleted();
+		timeToAssignment();
+		timeToTarget();
 	});
 });
 
-function timeToTargets(){
+function people(){
 	// for (var i = 0; i < allData.length; i++) {
 	// 	//for each user
 	// 	for (var j = 0; j < allData[i].data.length; j++) {
@@ -54,13 +56,15 @@ function timeToTargets(){
 function whenJoined(){
 	var joined = {};
 	for (var i = 0; i < allData.length; i++) {
-		var date = new Date(1000*allData[i].consent);
-		var month = date.getMonth()+1;
-		var day = date.getDate();
-		var year = date.getFullYear();
-		var datestring = ""+month+" "+day+" "+year;
-		if(joined[datestring]) joined[datestring] = joined[datestring]+1;
-		else joined[datestring] = 1;
+		if(allData[i].userId > lastDevUser){
+			var date = new Date(1000*allData[i].consent);
+			var month = date.getMonth()+1;
+			var day = date.getDate();
+			var year = date.getFullYear();
+			var datestring = ""+month+" "+day+" "+year;
+			if(joined[datestring]) joined[datestring] = joined[datestring]+1;
+			else joined[datestring] = 1;
+		}
 	};
 	var joinedArr = [];
 	for (var i = 0; i < Object.keys(joined).length; i++) {
@@ -137,9 +141,11 @@ function assignmentsCompleted(){
 		completed.push(temp);
 	};
 	for (var i = 0; i < allData.length; i++) {
-		for (var j = 0; j < allData[i].data.length; j++) {
-			completed[allData[i].data[j].assignment-1].count++;
-		};
+		if(allData[i].userId > lastDevUser){
+			for (var j = 0; j < allData[i].data.length; j++) {
+				completed[allData[i].data[j].assignment-1].count++;
+			};
+		}
 	};
 	//console.log(JSON.stringify(completed));
 
@@ -203,4 +209,219 @@ function assignmentsCompleted(){
 	  d.count = +d.count;
 	  return d;
 	}
+}
+
+function timeToTarget(){
+	var assignmentTargets = [];
+	for (var i = 0; i < assignments.length; i++) {
+		var temp = {"assignment":assignments[i].id, "targets":[]};
+		assignmentTargets.push(temp);
+	};
+	for (var i = 0; i < assignments.length; i++) {
+		for (var j = 0; j < assignments[i].targets.length; j++) {
+			assignmentTargets[i].targets.push([]);
+		};
+	};
+
+	for (var i = 0; i < allData.length; i++) {
+		if(allData[i].userId > lastDevUser){
+			for (var j = 0; j < allData[i].data.length; j++) {
+				//find the assignment (maybe more direct access via array)
+				for (var k = 0; k < assignmentTargets.length; k++) {
+					if(assignmentTargets[k].assignment == allData[i].data[j].assignment){
+						//found the assignment
+						//cross your fingers shit lines up right
+						for (var l = 0; l < allData[i].data[j].attempts.length; l++) {
+							// for (var m = 0; m < allData[i].data[j].attempts[l].length; m++) {
+							// 	allData[i].data[j].attempts[l][m]
+							// };
+							var last = allData[i].data[j].attempts[l][allData[i].data[j].attempts[l].length-1].time;
+							var first = allData[i].data[j].attempts[l][0].time;
+							var dur = last-first;
+							if(dur < 120){
+								assignmentTargets[k].targets[l].push(dur);
+							}
+						};
+					}
+				};
+			};
+		}
+	};
+	for (var i = 0; i < assignmentTargets.length; i++) {
+		for (var j = 0; j < assignmentTargets[i].targets.length; j++) {
+			var total = 0;
+			for (var k = 0; k < assignmentTargets[i].targets[j].length; k++) {
+				total += assignmentTargets[i].targets[j][k];
+			};
+			assignmentTargets[i].targets[j] = total/assignmentTargets[i].targets[j].length;
+		};
+	};
+
+	console.log(assignmentTargets);
+
+	var allTargets = [];
+	for (var i = 0; i < assignmentTargets.length; i++) {
+		for (var j = 0; j < assignmentTargets[i].targets.length; j++) {
+			if(assignmentTargets[i].targets[j] >-1){
+				allTargets.push(Math.round(1000*assignmentTargets[i].targets[j]));
+			}
+			else allTargets.push(0);
+		};
+	};
+	var allTargetsData = [];
+	for (var i = 0; i < allTargets.length; i++) {
+		var temp = {"time":allTargets[i], "which":i+1};
+		allTargetsData.push(temp);
+	};
+
+	// console.log(JSON.stringify(allTargetsData));
+	//
+	var margin = {top: 20, right: 20, bottom: 30, left: 60},
+	    width = (window.innerWidth) - margin.left - margin.right,
+	    height = (window.innerHeight) - margin.top - margin.bottom;
+
+	var x = d3.scale.ordinal()
+	    .rangeRoundBands([0, width], .1);
+
+	var y = d3.scale.linear()
+	    .range([height, 0]);
+
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left")
+	    .ticks(10, "");
+
+	var svg = d3.select("#timeToTarget").append("svg")
+	    .attr("width", "100%")
+	    .attr("height", "100%")
+	    .attr('viewBox','0 0 '+Math.min((width+margin.left+margin.right),(height+margin.top+margin.bottom))+' '+Math.min((width+margin.left+margin.right),(height+margin.top+margin.bottom)))
+	    .attr('preserveAspectRatio','xMinYMin')
+	  .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	var data = allTargetsData;
+	x.domain(data.map(function(d) { return d.which; }));
+	y.domain([0, d3.max(data, function(d) { return d.time; })]);
+
+	svg.append("g")
+	    .attr("class", "x axis")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(xAxis);
+
+	svg.append("g")
+	    .attr("class", "y axis")
+	    .call(yAxis)
+	  .append("text")
+	    .attr("transform", "rotate(-90)")
+	    .attr("y", -24)
+	    .attr("dy", ".71em")
+	    .style("text-anchor", "end")
+	    .text("Time to target");
+
+	svg.selectAll(".bar")
+	    .data(data)
+	  .enter().append("rect")
+	    .attr("class", "bar")
+	    .attr("x", function(d) { return x(d.which); })
+	    .attr("width", x.rangeBand())
+	    .attr("y", function(d) { return y(d.time); })
+	    .attr("height", function(d) { return height - y(d.time); });
+}
+
+function timeToAssignment(){
+	var assignmentTargets = [];
+	for (var i = 0; i < assignments.length; i++) {
+		var temp = {"assignment":assignments[i].id, "time":[]};
+		assignmentTargets.push(temp);
+	};
+	
+	for (var i = 0; i < allData.length; i++) {
+		if(allData[i].userId > lastDevUser){
+			for (var j = 0; j < allData[i].data.length; j++) {
+				//find the assignment (maybe more direct access via array)
+				for (var k = 0; k < assignmentTargets.length; k++) {
+					if(assignmentTargets[k].assignment == allData[i].data[j].assignment){
+						//found the assignment
+						//cross your fingers shit lines up right
+						var time = allData[i].data[j].finished - allData[i].data[j].started;
+						if(time < 1000){
+							assignmentTargets[k].time.push(time);
+						}
+					}
+				};
+			};
+		}
+	};
+	for (var i = 0; i < assignmentTargets.length; i++) {
+		var total = 0;
+		if(assignmentTargets[i].time.length == 0) assignmentTargets[i].time = 0;
+		else{
+			for (var j = 0; j < assignmentTargets[i].time.length; j++) {
+				total+=assignmentTargets[i].time[j];
+			};
+			assignmentTargets[i].time = total/assignmentTargets[i].time.length;
+		}
+	};
+
+
+	// console.log(JSON.stringify(assignmentTargets));
+	//
+	var margin = {top: 20, right: 20, bottom: 30, left: 60},
+	    width = (window.innerWidth) - margin.left - margin.right,
+	    height = (window.innerHeight) - margin.top - margin.bottom;
+
+	var x = d3.scale.ordinal()
+	    .rangeRoundBands([0, width], .1);
+
+	var y = d3.scale.linear()
+	    .range([height, 0]);
+
+	var xAxis = d3.svg.axis()
+	    .scale(x)
+	    .orient("bottom");
+
+	var yAxis = d3.svg.axis()
+	    .scale(y)
+	    .orient("left")
+	    .ticks(10, "");
+
+	var svg = d3.select("#timeToAssignment").append("svg")
+	    .attr("width", "100%")
+	    .attr("height", "100%")
+	    .attr('viewBox','0 0 '+Math.min((width+margin.left+margin.right),(height+margin.top+margin.bottom))+' '+Math.min((width+margin.left+margin.right),(height+margin.top+margin.bottom)))
+	    .attr('preserveAspectRatio','xMinYMin')
+	  .append("g")
+	    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+	var data = assignmentTargets;
+	x.domain(data.map(function(d) { return d.assignment; }));
+	y.domain([0, d3.max(data, function(d) { return d.time; })]);
+
+	svg.append("g")
+	    .attr("class", "x axis")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(xAxis);
+
+	svg.append("g")
+	    .attr("class", "y axis")
+	    .call(yAxis)
+	  .append("text")
+	    .attr("transform", "rotate(-90)")
+	    .attr("y", -24)
+	    .attr("dy", ".71em")
+	    .style("text-anchor", "end")
+	    .text("Time to target");
+
+	svg.selectAll(".bar")
+	    .data(data)
+	  .enter().append("rect")
+	    .attr("class", "bar")
+	    .attr("x", function(d) { return x(d.assignment); })
+	    .attr("width", x.rangeBand())
+	    .attr("y", function(d) { return y(d.time); })
+	    .attr("height", function(d) { return height - y(d.time); });
 }
